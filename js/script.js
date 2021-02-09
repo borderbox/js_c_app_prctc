86f5ae22e5362a87ac3125e07ce334fc5b6339fe
borderbox <tesenar@gmail.com>
2021-02-09 22:12:12 +0300

    function optimization. 17 lesson

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 'число фильмов'),
+let numberOfFilms;
 
- personalMovieDB = {
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'число фильмов');
+
+    while (numberOfFilms == '' || numberOfFilms === null || isNaN(numberOfFilms))  {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'число фильмов');
+    }   
+}
+
+start();
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,26 +20,34 @@ const numberOfFilms = prompt('Сколько фильмов вы уже посм
     privat: false
 };
  
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count == 10 || personalMovieDB.count <= 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count == 10 || personalMovieDB.count <= 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
 }
 
-for (let i = 0; i < 2; i++) {
-    let questionMovie = prompt('Один из последних просмотренных фильмов?', '');
-    while (questionMovie === '' || questionMovie === null || questionMovie.length > 50) {
-        questionMovie = prompt('Один из последних просмотренных фильмов?', '');
+detectPersonalLevel();
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let questionMovie = prompt('Один из последних просмотренных фильмов?', '');
+        while (questionMovie === '' || questionMovie === null || questionMovie.length > 50) {
+            questionMovie = prompt('Один из последних просмотренных фильмов?', '');
+        }
+        let questionRating = prompt('На сколько оцените его?', '');
+        while (questionRating === '' || questionRating === null || questionRating.length > 50) {
+            questionRating = prompt('На сколько оцените его?', '');
+        }
+        personalMovieDB.movies[questionMovie] = questionRating;
     }
-    let questionRating = prompt('На сколько оцените его?', '');
-    while (questionRating === '' || questionRating === null || questionRating.length > 50) {
-        questionRating = prompt('На сколько оцените его?', '');
-    }
-    personalMovieDB.movies[questionMovie] = questionRating;
 }
+
+rememberMyFilms();
 
 console.log(personalMovieDB);
